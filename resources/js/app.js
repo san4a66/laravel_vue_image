111738bd6da1047d6276260e25024e267fa0a533
San4a <racing-taz@e1.ru>
2022-07-03 10:32:42 +0500

    ADD Model Post,Image,component Index

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import {createApp} from 'vue'
+import Index from './components/Index'
+import router from './router'
+
+require('./bootstrap');
+
+createApp({
+    components:{Index}
+}).use(router).mount('#app')
---
+++ resources/js/router.js
@@ -0,0 +1,17 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+const routes = [
+    {
+        path: '/',
+        component: () => import('./components/Index'),
+        name: 'index'
+    }
+];
+
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
