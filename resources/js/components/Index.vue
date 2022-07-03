7e40d61fa30de13811b272e011682eb520c0d065
San4a <racing-taz@e1.ru>
2022-07-03 11:10:34 +0500

    Install dropzone.js

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,12 +1,28 @@
 <template>
-<div>
-  Index
+<div class="w-25">
+  <div ref="dropzone" class=" btn d-block p-5 bg-dark text-center text-light">
+    Uplolad
+  </div>
 </div>
 </template>
 
 <script>
+import Dropzone from 'dropzone'
 export default {
-  name: "Index"
+  name: "Index",
+
+  data(){
+    return {
+      dropzone:null
+    }
+  },
+
+  mounted(){
+    this.dropzone = new Dropzone(this.$refs.dropzone,{
+      url:"dfdfd",
+      clickable: true
+    })
+  }
 }
 </script>
 
